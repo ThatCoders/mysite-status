11fe9ed86b1a4030056e5b3356cb41e55d72062d
钟意 <thatcoder@163.com>
2023-08-30 17:17:08 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Stack Server Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2284369-4d6a92b8bc50aa409e9e2373'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Blog',
+      url: 'https://blog.thatcoder.cn/'
+    },
+    {
+      text: 'Laf',
+      url: 'https://laf.thatcoder.cn/'
+    },
+    {
+      text: 'Qmsg',
+      url: 'https://qmsg.thatcoder.cn/'
+    },
+  ],
+};
